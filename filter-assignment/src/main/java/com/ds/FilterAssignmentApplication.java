be5e535e6cfe108b6cae78eb991339845d467d1a
dattatrays29 <dattatrays1207@gmail.com>
2024-04-05 23:36:42 +0530

    init commit

---
+++ filter-assignment/src/main/java/com/ds/FilterAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.ds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilterAssignmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilterAssignmentApplication.class, args);
+	}
+
+}
